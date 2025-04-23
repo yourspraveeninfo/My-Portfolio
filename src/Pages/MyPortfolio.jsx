6110a1da561edbb984c717e234cd173851e883dd
iampraveenanthony <yourspraveenleo11@gmail.com>
2025-04-23 15:29:58 +0530

    django project

--- src/Pages/MyPortfolio.jsx
+++ src/Pages/MyPortfolio.jsx
@@ -4,7 +4,7 @@ import AmaImage from "../img/Amazon.jpg";
 import cicImage from "../img/Cisco.jpg";
 import busImage from "../img/Bus-Booking.png";
 import bankImage from "../img/banksystem.png";
-import cdbImage from "../img/cdb.png";
+import djaImage from "../img/django.jpg";
 
 
 export default function MyPortfolio() {
@@ -32,6 +32,33 @@ export default function MyPortfolio() {
           className="portfolio--section--card"
           onClick={() =>
             (window.location.href =
+              "https://github.com/yourspraveeninfo/Super_Mart_Django")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={djaImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Super Market | Django </h3>
+              <p className="text-md">
+              "Super Mart" is an e-commerce web application built using Django, MySQL, HTML, CSS, Bootstrap, JavaScript, and Python.
+              <br />It features Admin, Customer, Product, and Order Management with full CRUD functionality.
+              <br />Django’s built-in authentication system is used for secure admin login and access control.
+              <br />Data is efficiently handled through Django-MySQL integration for real-time access and updates.
+              </p>
+            </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/yourspraveeninfo/Super_Mart_Django">
+                View in GitHub
+              </a>
+            </p>
+          </div>
+        </div>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
               "https://yourspraveeninfo.github.io/Basic-Calculator/")
           }
         >
@@ -40,7 +67,7 @@ export default function MyPortfolio() {
           </div>
           <div className="portfolio--section--card--content">
             <div>
-              <h3 className="portfolio--section--title">Basic Calculator</h3>
+              <h3 className="portfolio--section--title">Calculator | Html, Css, JS</h3>
               <p className="text-md">
               This repository contains a simple calculator web application built using HTML, CSS, and JavaScript. The application provides a user-friendly interface for performing basic arithmetic operations, including addition, subtraction, multiplication, and division. It also supports percentage calculations, clear functionality, and a responsive design, ensuring a smooth and intuitive user experience.
               </p>
@@ -64,7 +91,7 @@ export default function MyPortfolio() {
           </div>
           <div className="portfolio--section--card--content">
             <div>
-              <h3 className="portfolio--section--title">Amazon Clone Website</h3>
+              <h3 className="portfolio--section--title">Amazon Website | React Js</h3>
               <p className="text-md">
               The website is a clone of Amazon, designed to showcase a shopping platform with a variety of products. It features product listings, a shopping cart, and a user-friendly interface. Users can browse electronics and other categories, add items to their cart, manage orders, and proceed to checkout, replicating Amazon's core functionality and layout.
               </p>
@@ -76,6 +103,8 @@ export default function MyPortfolio() {
             </p>
           </div>
         </div>
+      </div>
+      <div className="portfolio--section--container">
         <div
           className="portfolio--section--card"
           onClick={() =>
@@ -88,7 +117,7 @@ export default function MyPortfolio() {
           </div>
           <div className="portfolio--section--card--content">
             <div>
-              <h3 className="portfolio--section--title">Cisco Clone Website</h3>
+              <h3 className="portfolio--section--title">Cisco Website | Html, Css, JS</h3>
               <p className="text-md">
               I created a Cisco clone website using HTML, CSS, and JavaScript. The website is designed to showcase Cisco’s network, software, and cybersecurity solutions. It is not responsive and is optimized only for desktop viewing, ensuring a proper layout and functionality on larger screens without mobile compatibility.
               </p>
@@ -100,8 +129,6 @@ export default function MyPortfolio() {
             </p>
           </div>
         </div>
-      </div>
-      <div className="portfolio--section--container">
         <div
           className="portfolio--section--card"
           onClick={() =>
@@ -114,7 +141,7 @@ export default function MyPortfolio() {
           </div>
           <div className="portfolio--section--card--content">
             <div>
-              <h3 className="portfolio--section--title">Bus Booking System</h3>
+              <h3 className="portfolio--section--title">Bus Booking System | Pyhton</h3>
               <p className="text-md">
               The program is a bus booking system that enables users to register, log in, view available buses, and book tickets. Users enter personal details for registration and authentication. The system displays bus routes and schedules, allowing users to select a bus, confirm journey details, and complete the booking process seamlessly.
               </p>
@@ -138,37 +165,13 @@ export default function MyPortfolio() {
           </div>
           <div className="portfolio--section--card--content">
             <div>
-              <h3 className="portfolio--section--title">Banking System</h3>
+              <h3 className="portfolio--section--title">Banking System | Python</h3>
               <p className="text-md">
               This Python program simulates a simple banking system, enabling users to perform basic transactions. Users can check their balance, deposit funds, withdraw money, or exit the program. It includes input validation to prevent negative deposits and ensures sufficient balance for withdrawals, providing a secure and user-friendly banking experience.
               </p>
             </div>
             <p className="text-sm portfolio--link">
               <a href="https://github.com/yourspraveeninfo/Banking_System">
-                View in GitHub
-              </a>
-            </p>
-          </div>
-        </div>
-        <div
-          className="portfolio--section--card"
-          onClick={() =>
-            (window.location.href =
-              "https://github.com/yourspraveeninfo/College_DataBase")
-          }
-        >
-          <div className="portfolio--section--img">
-            <img src={cdbImage} alt="Placeholder" />
-          </div>
-          <div className="portfolio--section--card--content">
-            <div>
-              <h3 className="portfolio--section--title">College Database</h3>
-              <p className="text-md">
-              "The college database includes tables for student details, college bus information, and student mark statements, all managed using MySQL. It leverages SQL features like joins and grouping to efficiently retrieve and aggregate data across related tables. This structured approach enables comprehensive queries for generating detailed reports and insights."
-              </p>
-            </div>
-            <p className="text-sm portfolio--link">
-              <a href="https://github.com/yourspraveeninfo/College_DataBase">
                 View in GitHub
               </a>
             </p>
